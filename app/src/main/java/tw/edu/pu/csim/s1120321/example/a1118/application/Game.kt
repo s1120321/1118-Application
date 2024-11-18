9f767816120b8a8d48bcde80c00605a3cfba54b2
s1120321 <yieewenhong@gmail.com>
2024-11-18 16:30:23 +0800

    第1次commit

---
+++ app/src/main/java/tw/edu/pu/csim/s1120321/example/a1118/application/Game.kt
@@ -0,0 +1,32 @@
+package tw.edu.pu.csim.s1120321.example.a1118.application
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+
+
+
+class Game(val scope: CoroutineScope,val screenW:Int, val screenH: Int) {
+    var counter = 0
+    val state = MutableStateFlow(0)
+    val background = Background(screenW)
+
+
+    var isPlaying = true
+    fun Play(){
+        scope.launch {
+            //counter = 0
+            isPlaying = true
+            while (isPlaying) {
+                delay(40)
+                background.Roll()
+                counter++
+                state.emit(counter)
+
+            }
+        }
+    }
+
+}
+
